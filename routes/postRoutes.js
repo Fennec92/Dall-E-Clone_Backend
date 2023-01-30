67437541f1047d15c8f54784be1434143fb601cb
Fennec92 <nazar.hamidulla@posteo.de>
2023-01-30 18:46:10 +0100

    configured cloudinary for post route

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,8 +1,24 @@
 import "dotenv/config";
 import express from "express";
-import cloudinary from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import Post from "../mongodb/models/post.js";
 
 const postRouter = express.Router();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+postRouter.get("/", async (request, response) => {
+    try {
+    } catch (error) {}
+});
+
+postRouter.post("/", async (request, response) => {
+    try {
+    } catch (error) {}
+});
 
 export default postRouter;
